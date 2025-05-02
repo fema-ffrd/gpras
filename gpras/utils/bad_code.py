914afbefb30b5949b11504169e712967f49e9167
sclaw <klawson@dewberry.com>
2025-05-01 21:31:04 -0400

    bad code

--- gpras/utils/bad_code.py
+++ gpras/utils/bad_code.py
@@ -1,3 +1,11 @@
+# This is some code to fail both ruff and mypy
+
+
+def test(a, b: int) -> None:
+    c = a + b
+    return c
+
+
 """Docstring."""
 
 # This is some code to fail both ruff and mypy
