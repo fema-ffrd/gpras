cdd4b570f5166e04d5b56aa8b11b20d894b54aa3
sclaw <klawson@dewberry.com>
2025-05-09 21:08:40 +0000

    add first draft ras io

---
+++ gpras/utils/ras.py
@@ -0,0 +1,55 @@
+"""Functions to import and export HEC-RAS .hdf data and execute rasmapper."""
+
+import errno
+import os
+from functools import cached_property
+from typing import cast
+
+import numpy as np
+from hecstac.ras.assets import GeometryHdfAsset, PlanHdfAsset
+from hecstac.ras.item import RASModelItem
+from numpy.typing import NDArray
+from rashdf.geom import RasGeomHdf
+from rashdf.plan import RasPlanHdf, TimeSeriesOutputVar
+
+
+class RasPlan(RasPlanHdf):  # type: ignore[misc]
+    """OOP representation of a HEC-RAS plan hdf file."""
+
+    @cached_property
+    def mesh_names(self) -> list[str]:
+        """2D flow area names within the plan."""
+        return [i[0] for i in self._2d_flow_area_names_and_counts()]
+
+    def wsel_timeseries(self, mesh_id: str) -> NDArray[np.float64]:
+        """Get timeseries of water surface elevation for all cells."""
+        ts = self._mesh_timeseries_outputs(mesh_id, TimeSeriesOutputVar)
+        return cast(NDArray[np.float64], ts["Water Surface"].values)
+
+
+class RasModel(RASModelItem):  # type: ignore[misc]
+    """OOP representation of a HEC-RAS project."""
+
+    @cached_property
+    def plan_hdfs(self) -> dict[str, RasPlan]:
+        """List the available plan hdfs."""
+        hdfs = [i for i, j in self.assets.items() if isinstance(j, PlanHdfAsset)]
+        hdfs = [RasPlan(self.pm.derived_item_asset(i)) for i in hdfs]
+        return {i.get_attrs(i.PLAN_INFO_PATH)["Plan Title"]: i for i in hdfs}
+
+    @cached_property
+    def geom_hdfs(self) -> dict[str, RasGeomHdf]:
+        """List the available plan hdfs."""
+        hdfs = [i for i, j in self.assets.items() if isinstance(j, GeometryHdfAsset)]
+        hdfs = [RasGeomHdf(self.pm.derived_item_asset(i)) for i in hdfs]
+        return {i.get_geom_attrs()["Title"]: i for i in hdfs}
+
+    def get_plan_geometry(self, plan_name: str) -> RasGeomHdf:
+        """Return the RasGeomHdf for a RasPlanHdf."""
+        plan = self.plan_hdfs[plan_name]
+        geom = plan.get_attrs(plan.PLAN_INFO_PATH)["Geometry Title"]
+        if geom not in self.geom_hdfs:
+            err = errno.ENOENT
+            missing_file = plan.get_attrs(plan.PLAN_INFO_PATH)["Geometry Filename"]
+            raise FileNotFoundError(err, os.strerror(err), missing_file)
+        return self.geom_hdfs[geom]
---
+++ tests/ras/hdf_files.py
@@ -0,0 +1,32 @@
+"""Test I/O for Ras .hdf files."""
+
+from gpras.utils.ras import RasModel
+
+TEST_PRJ = "data/Muncie/Muncie.prj"
+
+
+def test_ras_plan_hdf() -> None:
+    """Load a model and iterate through plan files."""
+    model = RasModel.from_prj(TEST_PRJ, "model", "EPSG:2965")
+    for i in model.plan_hdfs:
+        plan = model.plan_hdfs[i]
+        geom = model.get_plan_geometry(i)
+        assert geom is not None
+        meshes = plan._2d_flow_area_names_and_counts()
+        for j in meshes:
+            summary = plan.wsel_timeseries(j[0])
+            assert summary is not None
+
+
+def test_ras_geom_hdf() -> None:
+    """Load a model and iterate through geometry files."""
+    model = RasModel.from_prj(TEST_PRJ, "model", "EPSG:2965")
+    for i in model.geom_hdfs:
+        geom = model.geom_hdfs[i]
+        meshes = geom.mesh_cell_polygons()
+        assert meshes is not None
+
+
+if __name__ == "__main__":
+    test_ras_plan_hdf()
+    test_ras_geom_hdf()
