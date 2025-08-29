f0520f23cc7edb45e1104d6f4aac96b9bae51650
sclaw <klawson@dewberry.com>
2025-08-29 20:27:04 +0000

    overhaul fitting methods

--- gpras/gpr.py
+++ gpras/gpr.py
@@ -4,15 +4,18 @@ from __future__ import annotations
 
 import json
 from pathlib import Path
-from typing import Any, Literal, Self, TypeVar
+from typing import Any, Literal, Self, TypeVar, cast
 
 import gpflow
 import numpy as np
 import tensorflow as tf
+import tensorflow_probability as tfp
 from gpflow.models import SGPR
 from numpy.typing import NDArray
+from scipy.optimize import differential_evolution
 from sklearn.cluster import KMeans
 
+gpflow.config.set_default_float(tf.float64)
 T = TypeVar("T", bound="GPRAS")
 
 KERNEL_FACTORY = {
@@ -34,6 +37,131 @@ KERNEL_FACTORY = {
 }
 
 KernelType = Literal["Matern12", "Matern32", "Matern52", "RBF", "Linear", "Polynomial", "Periodic", "Exponential"]
+OptimizerType = Literal["two-stage", "adam", "L-BFGS-B", "stochastic", "diffential_evolution"]
+InductionInitializerType = Literal["kmeans", "grid"]
+
+
+def _optimize_differential_evolutions(model: SGPR, popsize: int = 15, maxiter: int = 500) -> None:
+    """Use differential evolution to optimize any trainable model parameters."""
+    # Hyperparameter bounds
+    param_bounds = [(-1, 1), (-1, 1), (-3, 0)]
+
+    # Inducing variable bounds
+    x = model.data[0].numpy()
+    z_shape = model.inducing_variable.Z.shape
+    mins, maxs = x.min(axis=0), x.max(axis=0)
+    for d in range(z_shape[1]):
+        param_bounds.extend([(mins[d], maxs[d])] * z_shape[0])
+
+    # Define objective
+    def objective(params: list[NDArray[Any]]) -> Any:
+        model.kernel.variance.assign(10 ** params[0])
+        model.kernel.lengthscales.assign(10 ** params[1])
+        model.likelihood.variance.assign(10 ** params[2])
+        model.inducing_variable.Z = np.array(params[3:]).reshape(z_shape)
+
+        print(model.training_loss().numpy())
+        return model.training_loss().numpy()
+
+    # Run optimization
+    result = differential_evolution(objective, param_bounds, popsize=popsize, maxiter=maxiter)
+
+    # Set results
+    model.kernel.variance.assign(10 ** result.x[0])
+    model.kernel.lengthscales.assign(10 ** result.x[1])
+    model.likelihood.variance.assign(10 ** result.x[2])
+    model.inducing_variable.Z = np.array(result.x[3:]).reshape(z_shape)
+
+
+def _optimize_multi_start(model: SGPR, n_starts: int = 40, iter_initial: int = 20, iter_final: int = 1000) -> None:
+    """Run an initial coarse parameter optimization, identify the best parameters, then fine tune."""
+    # Initialize random number generator
+    np.random.seed(1)
+    rng = np.random.default_rng()
+
+    # Define shapes of hyperparameters
+    x = model.data[0].numpy()
+    z = model.inducing_variable.Z
+    mins, maxs = x.min(axis=0), x.max(axis=0)
+    z_shape = (z.shape[0], x.shape[1])
+
+    # Multiple starts
+    best_loss = None
+    for _ in range(n_starts):
+        model.kernel.variance.assign(10 ** rng.uniform(-1, 1))
+        model.kernel.lengthscales.assign(10 ** rng.uniform(-1, 1))
+        model.likelihood.variance.assign(10 ** rng.uniform(-3, 0))
+        model.inducing_variable.Z = rng.uniform(mins, maxs, size=z_shape)
+
+        _optimize_adam(model, iter_initial)
+
+        loss = model.training_loss().numpy()
+        if best_loss is None or loss < best_loss:
+            best_params = [
+                model.kernel.variance.numpy(),
+                model.kernel.lengthscales.numpy(),
+                model.likelihood.variance.numpy(),
+                model.inducing_variable.Z,
+            ]
+
+    # Final tuning
+    model.kernel.variance.assign(best_params[0])
+    model.kernel.lengthscales.assign(best_params[1])
+    model.likelihood.variance.assign(best_params[2])
+    model.inducing_variable.Z = best_params[3]
+    _optimize_bfgs(model, iter_final)
+
+
+def _optimize_two_stage(model: SGPR, max_iter: int = 100) -> None:
+    """Use Adam algorithm to optimize any trainable model parameters but optimizing inducing points first."""
+    # Optimize inducing points
+    gpflow.set_trainable(model, False)
+    gpflow.set_trainable(model.inducing_variable.Z, True)
+    _optimize_adam(model, max_iter)
+
+    # Optimize other parameters
+    gpflow.set_trainable(model, True)
+    gpflow.set_trainable(model.inducing_variable.Z, False)
+    _optimize_adam(model, max_iter)
+
+    # Cleanup
+    gpflow.set_trainable(model.inducing_variable.Z, True)
+
+
+def _optimize_adam(model: SGPR, max_iter: int) -> None:
+    """Use Adam algorithm to optimize any trainable model parameters."""
+    opt = tf.keras.optimizers.Adam()
+
+    @tf.function  # type: ignore[misc]
+    def step() -> tf.Tensor:
+        with tf.GradientTape() as tape:
+            loss = model.training_loss()
+        grads = tape.gradient(loss, model.trainable_variables)
+        opt.apply_gradients(zip(grads, model.trainable_variables, strict=False))
+        return loss
+
+    for _ in range(max_iter):
+        step()
+
+
+def _optimize_bfgs(model: SGPR, max_iter: int) -> None:
+    """Use Scipy Limited-memory BFGS (gradient descent) to optimize any trainable model parameters."""
+    opt = gpflow.optimizers.Scipy()
+    opt.minimize(
+        model.training_loss,
+        model.trainable_variables,
+        method="L-BFGS-B",
+        options={"maxiter": max_iter},
+    )
+
+
+OPTIMIZERS: dict[str, Any] = {
+    "two-stage": _optimize_two_stage,
+    "adam": _optimize_adam,
+    "L-BFGS-B": _optimize_bfgs,
+    "stochastic": _optimize_multi_start,
+    "diffential_evolution": _optimize_differential_evolutions,
+}
 
 
 class GPRAS:
@@ -53,66 +181,90 @@ class GPRAS:
 
         self.models: list[SGPR] = []
 
-    def fit(self, x: NDArray[Any], y: NDArray[Any], inducing_fraction: float) -> None:
+    def fit(
+        self,
+        x: NDArray[Any],
+        y: NDArray[Any],
+        n_inducing: int,
+        inducing_initializer: InductionInitializerType = "kmeans",
+        optimization_method: OptimizerType = "two-stage",
+        **opt_kwargs: dict[Any, Any],
+    ) -> None:
         """Fit the Gaussian Process Regression model to observed data.
 
         Args:
             x (NDArray[Any]): The input data of shape (n_samples, n_features).
             y (NDArray[Any]): The target data of shape (n_samples, n_outputs).
-            inducing_fraction (float): Fraction of the data to use as inducing points. This can tune training speed.
+            n_inducing (int): Number of inducing points to use for sparse approximation.
+            inducing_initializer (InductionInitializerType, optional):
+                Strategy for initializing inducing points (e.g., "kmeans", "random").
+                Defaults to "kmeans".
+            optimization_method (OptimizerType, optional):
+                Optimization method to use for training
+                (e.g., "adam", "l-bfgs", "two-stage"). Defaults to "two-stage".
+            **opt_kwargs (dict[str, Any], optional):
+                Additional keyword arguments passed to the optimizer.
 
         Returns:
             None
         """
-        # Cast variables to float64 for gpflow
+        # Cast data to float64
         x = x.astype(np.float64)
         y = y.astype(np.float64)
 
-        # Initialize inducing points with kmeans
-        km = KMeans(n_clusters=round(x.shape[0] * inducing_fraction), random_state=0, n_init="auto")
-        km.fit(x)
-        inducing_variable = km.cluster_centers_.astype(np.float64)
+        # Create base models
+        self._init_models(x, y, n_inducing, inducing_initializer)
 
-        # Define gpr training optimizer
+        # Optimize hyperparameters
+        opt = OPTIMIZERS[optimization_method]
+        for i in self.models:
+            opt(i, **opt_kwargs)
+            # self._optimize_adam(i)
+            # self._multi_start_gradient_descent(i)
+            # self._optimize_differential_evolutions(i, 5, 500)
+            # self._optimize_two_stage(i, max_iter=500)
+            print(i.training_loss().numpy())
 
-        # Train GPR models
+    def _init_models(
+        self,
+        x: NDArray[Any],
+        y: NDArray[Any],
+        n_inducing: int,
+        inducing_initializer: InductionInitializerType = "kmeans",
+    ) -> None:
+        """Create one model per spatial mode using base model settings."""
+        # Make an inducing variable
+        inducing = self._create_inducing(x, n_inducing, inducing_initializer)
+
+        # Create models for each spatial mode
         self.models = []
         for i in range(y.shape[1]):
-            # Subset data & instantiate GPR
+            # Subset to one spatial mode
             y_i = np.c_[y[:, i]]
+
+            # Initialize model
             kernel_i = self.kernel()
-            model_i = SGPR(data=(x, y_i), kernel=kernel_i, inducing_variable=inducing_variable)
-            options = {"maxiter": 500, "gtol": 1e-6, "ftol": 1e-9}
+            model = SGPR(data=(x, y_i), kernel=kernel_i, inducing_variable=inducing)
 
-            # Optimize inducing points
-            gpflow.set_trainable(model_i.kernel.variance, False)
-            gpflow.set_trainable(model_i.kernel.lengthscales, False)
-            gpflow.set_trainable(model_i.likelihood.variance, False)
+            # Set priors to avoid pathological models (hopefully)
+            model.kernel.variance.prior = tfp.distributions.LogNormal(gpflow.utilities.to_default_float(0), 1.0)
+            model.kernel.lengthscales.prior = tfp.distributions.LogNormal(gpflow.utilities.to_default_float(0), 1.0)
+            model.likelihood.variance.prior = tfp.distributions.LogNormal(gpflow.utilities.to_default_float(0), 1.0)
 
-            opt = gpflow.optimizers.Scipy()
+            # Add model to list
+            self.models.append(model)
 
-            result = opt.minimize(
-                model_i.training_loss,
-                model_i.trainable_variables,
-                method="L-BFGS-B",
-                options=options,
-            )
-            print("Number of iterations:", result.nit)
-
-            # Optimize kernel and model parameters
-            gpflow.set_trainable(model_i.kernel.variance, True)
-            gpflow.set_trainable(model_i.kernel.lengthscales, True)
-            gpflow.set_trainable(model_i.likelihood.variance, True)
-            gpflow.set_trainable(model_i.inducing_variable.Z, False)
-
-            result = opt.minimize(
-                model_i.training_loss, model_i.trainable_variables, method="L-BFGS-B", options=options
-            )
-            print("Number of iterations:", result.nit)
-            print()
-
-            # Log model
-            self.models.append(model_i)
+    def _create_inducing(self, x: NDArray[Any], n_inducing: int, method: InductionInitializerType) -> NDArray[Any]:
+        """Create an array representing locations in dataspace."""
+        if method == "kmeans":
+            km = KMeans(n_clusters=n_inducing, random_state=0, n_init="auto")
+            km.fit(x)
+            return cast(NDArray[Any], km.cluster_centers_.astype(np.float64))
+        elif method == "grid":
+            inducing_variable = np.c_[np.linspace(x[:, 0].min(), x[:, 0].max(), n_inducing)]
+            for j in range(1, x.shape[1]):
+                inducing_variable = np.c_[inducing_variable, np.linspace(x[:, j].min(), x[:, j].max(), n_inducing)]
+            return cast(NDArray[Any], inducing_variable)
 
     def predict(self, x: NDArray[Any]) -> tuple[NDArray[Any], NDArray[Any]]:
         """Predict high-fidelity data using the trained Gaussian Process Regression model.
